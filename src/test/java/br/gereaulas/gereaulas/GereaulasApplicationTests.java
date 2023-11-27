10ca91ee4aec97dfea28fc1abc7ac4ef11a27e4d
Rita Lopes <rita.lopes@smartsuite.imd.ufrn.br>
2023-11-27 03:22:48 -0300

    skip test

--- src/test/java/br/gereaulas/gereaulas/GereaulasApplicationTests.java
+++ src/test/java/br/gereaulas/gereaulas/GereaulasApplicationTests.java
@@ -1,13 +1,16 @@
 package br.gereaulas.gereaulas;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import br.gereaulas.app.GereaulasApplication;
-
-@SpringBootTest(classes=GereaulasApplication.class
-)
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = {
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.liquibase.enabled=false",
+		"spring.flyway.enabled=false"
+})
 class GereaulasApplicationTests {
 
 	@Test
